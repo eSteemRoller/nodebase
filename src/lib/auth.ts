a09254928f8c8485056d06bc0d84ee6e337ec421
Jason <jasondoroughonfb@live.com>
2025-11-02 23:30:29 -0800

    17: setup Polar, Part 2, integrate Polar with Better Auth, access to checkout and billing portal

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,9 @@
 
+import { checkout, polar, portal } from '@polar-sh/better-auth';
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./db";
+import { polarClient } from './polar';
 
 
 export const auth = betterAuth({
@@ -12,4 +14,23 @@ export const auth = betterAuth({
       enabled: true,
       autoSignIn: true,
     },
+    plugins: [ 
+      polar({ 
+        client: polarClient,
+        createCustomerOnSignUp: true,
+        use: [ 
+          checkout({ 
+            products: [
+              { 
+                productId: 'e4d60148-f065-4ab9-b66e-761a4a0e73ff',
+                slug: 'Nodebase-Pro',
+              }
+            ],
+            successUrl: process.env.POLAR_SUCCESS_URL,
+            authenticatedUsersOnly: true,
+          }),
+          portal()
+        ],
+      })
+    ]
 });
