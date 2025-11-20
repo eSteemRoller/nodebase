bd1f82ea6a3ab857aac9a35f945606e8d02f5fdd
Jason <jasondoroughonfb@live.com>
2025-11-19 23:00:01 -0800

    33: installed ParallelDrive; Node selector, Part 1

--- src/features/editor/components/add-node-button.tsx
+++ src/features/editor/components/add-node-button.tsx
@@ -1,21 +1,26 @@
 
 'use client';
 
-import { memo } from 'react';
+import { memo, useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { PlusIcon } from 'lucide-react';
+import { NodeSelector } from '@/components/node-selector';
 
 
 export const AddNodeButton = memo(() => { 
+  const [selectorOpen, setSelectorOpen] = useState(false);
+
   return ( 
-    <Button 
-      onClick={() => {}}
-      size='icon'
-      variant='outline'
-      className='bg-background'
-    >
-      <PlusIcon />
-    </Button>
+    <NodeSelector open={selectorOpen} onOpenChange={setSelectorOpen} >
+      <Button 
+        onClick={() => {}}
+        size='icon'
+        variant='outline'
+        className='bg-background'
+      >
+        <PlusIcon />
+      </Button>
+    </NodeSelector>
   )
 });
 
