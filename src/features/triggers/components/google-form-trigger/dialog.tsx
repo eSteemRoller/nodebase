057ff4aebc865ef193ae7f256b5a5a2c18a964f1
Jason <jasondoroughonfb@live.com>
2025-12-01 22:13:46 -0800

    45: Google Form trigger, Part 3

--- src/features/triggers/components/google-form-trigger/dialog.tsx
+++ src/features/triggers/components/google-form-trigger/dialog.tsx
@@ -92,15 +92,19 @@ export const GoogleFormTriggerDialog = ({
               list-decimal 
               list-inside'
             >
-              <li>Open your Google Form</li>
-              <li>Click on the ellipsis (...) menu</li>
-              <li>Click on Script editor</li>
-              <li>Click the Copy Google Apps Script button below</li>
-              <li>To the right of "WEBHOOK_URL" replace "webhookUrl" with the webhook URL above (without quotes)</li>
-              <li>Save your Script</li>
-              <li>Click on "Triggers"</li>
-              <li>Click on "Add Trigger"</li>
-              <li>Click on "From form"</li>
+              <li>Login to your Google account and open your Google Form</li>
+              <li>Click on the ellipsis (...) menu near the top right corner</li>
+              <li>Click on "Apps Script"</li>
+              <li>Click the "Copy Google Apps Script" button below</li>
+              <li>Paste the script in the scriptwriting area, completely overwriting the "myFunction" default script function template.</li>
+              <li>To the right of "WEBHOOK_URL" verify if the current webhook URL is the one you want to use.</li>
+              <li>If the "WEBHOOK_URL" is missing or problematic, you can replace "webhookUrl" with the webhook URL above (without quotes)</li>
+              <li>Click on the floppy disk icon to "Save project to Drive"</li>
+              <li>Click on "Triggers" (the clock icon) in the left slideout menu</li>
+              <li>Click on the "Add Trigger" button near the bottom right corner</li>
+              <li>Verify the trigger settings.</li>
+              <li>For this example, we are using the event source, "From form," the event type, "On form submit," and the failure notification setting, "Notify me immediately"</li>
+              <li>Save the trigger.</li>
               <li>Click on "On form submit"</li>
               <li>Save your Trigger</li>
             </ol>
@@ -130,7 +134,7 @@ export const GoogleFormTriggerDialog = ({
               Copy Google Apps Script
             </Button>
             <p className='text-xs text-muted-foreground'>
-              This script includes your webhook URL and it 
+              This script includes the Webhook URL above and it 
               handles form submissions
             </p>
           </div>
